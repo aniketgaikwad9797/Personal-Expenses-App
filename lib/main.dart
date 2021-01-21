b8bb733778d6eb74c0282dec0fbe8951005228d7
aniketgaikwad9797 <aniketgaikwad9797@users.noreply.github.com>
2021-01-22 03:00:53 +0530

    create custom transaction widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:PersonalExpenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'models/transaction.dart';
+
+
 
 void main() {
   runApp(MyApp());
@@ -25,20 +24,6 @@ class MyHomePage extends StatelessWidget {
   //String amountinput;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
-  final List<Transaction> transactions = [
-    Transaction(
-      id: "1",
-      name: "Clothes",
-      amount: 1200,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "2",
-      name: "Shoes",
-      amount: 1400,
-      date: DateTime.now(),
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,63 +73,6 @@ class MyHomePage extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 10,
-                      ),
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                        ),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Text(
-                        "Rs: ${tx.amount}", //String Interpolation
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          child: Text(
-                            tx.name,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          child: Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(
-                              color: Colors.grey[400],
-                              fontSize: 16,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(), //map() method is used to convert list of objects into list of widgets
-            //toList() is used bcoz map() function will always give us a iterable
-            //which we need to iterate
-            //also map() takes a function which gets automatically executed
           ),
         ],
       ),
