761d08a6a3c862c7ccc8767a013d9d81600d5715
aniketgaikwad9797 <aniketgaikwad9797@users.noreply.github.com>
2021-01-22 18:30:12 +0530

    use builder consructor of ListView

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -8,62 +8,68 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 10,
-                ),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 10,
                   ),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Text(
-                  "Rs: ${tx.amount}", //String Interpolation
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    child: Text(
-                      tx.name,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  child: Text(
+                    "Rs: ${transactions[index].amount}", //String Interpolation
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
                   ),
-                  Container(
-                    child: Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(
-                        color: Colors.grey[400],
-                        fontSize: 16,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        transactions[index].name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  )
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(), //map() method is used to convert list of objects into list of widgets
-      //toList() is used bcoz map() function will always give us a iterable
-      //which we need to iterate
-      //also map() takes a function which gets automatically executed
+                    Container(
+                      child: Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 16,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+        //children: transactions.map((tx) {
+        // return ).toList(), //map() method is used to convert list of objects into list of widgets
+        //toList() is used bcoz map() function will always give us a iterable
+        //which we need to iterate
+        //also map() takes a function which gets automatically executed
+      ),
     );
   }
 }
