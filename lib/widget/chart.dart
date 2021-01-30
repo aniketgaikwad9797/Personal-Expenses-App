a07f491bb06990c5e0e2e5bd3142f718a5f09f81
aniketgaikwad9797 <aniketgaikwad9797@users.noreply.github.com>
2021-01-31 02:05:08 +0530

    add ternary operator for total spending

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -48,7 +48,9 @@ class Chart extends StatelessWidget {
           return ChartBar(
             data["day"],
             data["amount"],
-            (data["amount"] as double)/totalSpending,
+            totalSpending == 0.0
+                ? 0.0
+                : (data["amount"] as double) / totalSpending,
           );
         }).toList(),
       ),
